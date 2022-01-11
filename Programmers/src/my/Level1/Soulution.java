cf09419d8138bfe56f74d78042691f5428a59cba
m16khb <m16khb@gmail.com>
2022-01-11 10:20:21 +0900

    주석 수정

---
+++ Programmers/src/my/Level1/Soulution.java
@@ -0,0 +1,11 @@
+package my.Level1;
+
+public class Soulution {
+	public int solution(int[][] board, int[] moves) {
+        int answer = 0;
+        return answer;
+    }
+	public static void main(String[] args) {
+
+	}
+}
