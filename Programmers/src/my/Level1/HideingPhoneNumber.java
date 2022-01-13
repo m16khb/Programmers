feebf055a16dd60280a328021c201fde9dfd73f8
m16khb <m16khb@gmail.com>
2022-01-13 18:58:46 +0900

    스터디 3문제 완료

---
+++ Programmers/src/my/Level1/HideingPhoneNumber.java
@@ -0,0 +1,21 @@
+package my.Level1;
+
+public class HideingPhoneNumber {
+
+	public static void main(String[] args) {
+		// TODO 자동 생성된 메소드 스텁
+
+	}
+    public String solution(String phone_number) {
+        String answer = "";
+        for (int i = 0; i < phone_number.length(); i++) {
+			if(phone_number.length() - i > 4) {
+				answer += "*";
+			}
+			else {
+				answer += phone_number.charAt(i);
+			}
+		}
+        return answer;
+    }
+}
