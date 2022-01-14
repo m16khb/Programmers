7a097a0b97de80f75ca1b499d56ac6f2e19a8814
m16khb <m16khb@gmail.com>
2022-01-14 15:49:39 +0900

    음양 더하기

---
+++ Programmers/src/my/Level1/MinusPlusSum.java
@@ -0,0 +1,17 @@
+package my.Level1;
+
+public class MinusPlusSum {
+	public static void main(String[] args) {
+		
+	}
+    public int solution(int[] absolutes, boolean[] signs) {
+        int answer = 0;
+        for (int i = 0; i < signs.length; i++) {
+			if(signs[i]) 
+				answer += absolutes[i];
+			else
+				answer -= absolutes[i];
+		}
+        return answer;
+    }
+}
