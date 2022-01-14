84c5399d335c464273a8ae36debe85bec15e5f28
m16khb <m16khb@gmail.com>
2022-01-14 15:44:49 +0900

    업는 숫자 더하기

---
+++ Programmers/src/my/Level1/NoNumberCheck.java
@@ -0,0 +1,27 @@
+package my.Level1;
+
+public class NoNumberCheck {
+
+	public static void main(String[] args) {
+		// TODO 자동 생성된 메소드 스텁
+
+	}
+    public int solution(int[] numbers) {
+        int answer = -1;
+        answer++;
+        
+        for (int i = 1; i < 10; i++) {
+        	boolean flag = true;
+			for (int j = 0; j < numbers.length; j++) {
+				if(numbers[j] == i) 
+					flag = false;
+			}
+			if(flag) {
+				answer += i;
+			}
+			flag = true;
+		}
+        
+        return answer;
+    }
+}
