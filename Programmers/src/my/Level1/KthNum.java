d97fa7a5ca940523d7ddc7d2b24bc5c04f8ffd9b
m16khb <m16khb@gmail.com>
2022-01-16 23:06:04 +0900

    K번째 수

--- Programmers/src/my/Level1/KthNum.java
+++ Programmers/src/my/Level1/KthNum.java
@@ -1,21 +1,41 @@
 package my.Level1;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class KthNum {
 
 	public static void main(String[] args) {
 		// TODO 자동 생성된 메소드 스텁
-
+		KthNum kn = new KthNum();
+		int[] n = {1, 5, 2, 6, 3, 7, 4};
+		int[][] k = {{2, 5, 3},{4, 4, 1},{1, 7, 3}};
+		int[] answer = kn.solution(n, k);
+		for (int i = 0; i < answer.length; i++) {			
+			System.out.print(answer[i] + " ");
+		}
 	}
-    public int[] solution(int[] array, int[][] commands) {
-        int[] answer = {};
-        ArrayList<Integer> arr = new ArrayList<>();
-        
-        for (int i : array) {
+
+	public int[] solution(int[] array, int[][] commands) {
+		int[] answer = {};
+		List<Integer> arr = new ArrayList<>();
+
+		for (int i : array) {
 			arr.add(i);
 		}
-        
-        return answer;
-    }
+
+		answer = new int[commands.length];
+
+		for (int i = 0; i < commands.length; i++) {
+			List<Integer> temp;
+			//이렇게 하면 sort시 arr도 sort됨 얕은복사? 일듯
+			//temp = arr.subList(commands[i][0]-1, commands[i][1]); 
+			temp = new ArrayList<Integer>(arr.subList(commands[i][0]-1, commands[i][1]));
+			Collections.sort(temp);
+			answer[i] = temp.get(commands[i][2] - 1);
+		}
+
+		return answer;
+	}
 }
