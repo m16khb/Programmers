03bd7948fc2cf0b841918d86ed1de86d6b34ee17
m16khb <m16khb@gmail.com>
2022-01-20 22:24:56 +0900

    짝수와 홀수

--- Programmers/src/my/Level1/EvenOdd.java
+++ Programmers/src/my/Level1/EvenOdd.java
@@ -1,5 +1,9 @@
 package my.Level1;
 
 public class EvenOdd {
-
+	public String solution(int num) {
+        String answer = "";  
+        answer = num % 2 == 0 ? "Even": "Odd";
+        return answer;
+    }
 }
