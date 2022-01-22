10f206b310fbd06839c6782466b3536a13be3bd2
m16khb <m16khb@gmail.com>
2022-01-22 23:28:47 +0900

    제일 작은 수 제거하기

--- Programmers/src/my/Level1/MostSmallNumEliminate.java
+++ Programmers/src/my/Level1/MostSmallNumEliminate.java
@@ -1,5 +1,36 @@
 package my.Level1;
 
 public class MostSmallNumEliminate {
+	public int[] solution(int[] arr) {
+		int[] answer = {};
 
+		int min = arr[0];
+		for (int i = 1; i < arr.length; i++) {
+			if (min > arr[i])
+				min = arr[i];
+		}
+
+		int count = 0;
+		for (int num : arr) {
+			if (num > min)
+				count++;
+		}
+
+		if (count == 0) {
+			answer = new int[1];
+			answer[0] = -1;
+		} else {
+			answer = new int[count];
+		}
+
+		int i = 0;
+		for (int num : arr) {
+			if (num > min) {
+				answer[i] = num;
+				i++;
+			}
+		}
+
+		return answer;
+	}
 }
