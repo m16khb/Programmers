d9f0c2a5d3e022a60e1c923c9a065fc251e0fc92
m16khb <m16khb@gmail.com>
2022-01-24 00:45:58 +0900

    정수 제곱근 판별

--- Programmers/src/my/Level1/IntegerSquareRoot.java
+++ Programmers/src/my/Level1/IntegerSquareRoot.java
@@ -3,8 +3,18 @@ package my.Level1;
 public class IntegerSquareRoot {
 
 	public static void main(String[] args) {
-		// TODO 자동 생성된 메소드 스텁
 
 	}
-
+    public long solution(long n) {
+        long answer = 0;
+        
+        double k = Math.sqrt(n);
+        if((long)(k)*(long)(k) == n) {
+        	answer = (long)(k+1) * (long)(k+1);
+        }else {
+        	answer = -1;
+        }
+        
+        return answer;
+    }
 }
