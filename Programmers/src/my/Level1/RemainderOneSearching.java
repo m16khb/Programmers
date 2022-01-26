8cefd543072881a31286cc646622b616515e55c4
m16khb <m16khb@gmail.com>
2022-01-26 21:40:37 +0900

    나머지가 1이 되는 수 찾기

---
+++ Programmers/src/my/Level1/RemainderOneSearching.java
@@ -0,0 +1,17 @@
+package my.Level1;
+
+public class RemainderOneSearching {
+
+	public static void main(String[] args) {
+
+	}
+
+	public int solution(int n) {
+		int answer = 0;
+
+		while ((n % ++answer) != 1) {
+		}
+
+		return answer;
+	}
+}
