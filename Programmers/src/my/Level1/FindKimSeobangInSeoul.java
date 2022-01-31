1cf749170efd799f414cbc943fc980b2b5b2ce87
m16khb <m16khb@gmail.com>
2022-01-31 16:42:17 +0900

    서울에서 김서방 찾기

---
+++ Programmers/src/my/Level1/FindKimSeobangInSeoul.java
@@ -0,0 +1,20 @@
+package my.Level1;
+
+public class FindKimSeobangInSeoul {
+
+	public static void main(String[] args) {
+
+	}
+
+	public String solution(String[] seoul) {
+		String answer = "";
+		
+		for (int i = 0; i < seoul.length; i++) {
+			if(seoul[i].equals("Kim")) {
+				answer += "김서방은 " + i + "에 있다";
+			}
+		}
+		
+		return answer;
+	}
+}
