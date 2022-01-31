bbfcb634c1fd4872945517724cc7af50307baed8
m16khb <m16khb@gmail.com>
2022-01-31 17:04:58 +0900

    문자열을 정수로 바꾸기

---
+++ Programmers/src/my/Level1/ConvertStringToInteger.java
@@ -0,0 +1,16 @@
+package my.Level1;
+
+public class ConvertStringToInteger {
+
+	public static void main(String[] args) {
+
+	}
+
+	public int solution(String s) {
+		int answer = 0;
+		
+		answer = Integer.parseInt(s);
+		
+		return answer;
+	}
+}
