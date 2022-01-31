9241425fa0408447a2a04e4fa840331fb947be94
m16khb <m16khb@gmail.com>
2022-01-31 17:21:52 +0900

    약수의 합

---
+++ Programmers/src/my/Level1/SumOfFactors.java
@@ -0,0 +1,20 @@
+package my.Level1;
+
+public class SumOfFactors {
+
+	public static void main(String[] args) {
+
+	}
+
+	public int solution(int n) {
+		int answer = 0;
+		
+		for (int i = 1; i < n + 1; i++) {
+			if(n % i == 0) {
+				answer += i;
+			}
+		}
+		
+		return answer;
+	}
+}
