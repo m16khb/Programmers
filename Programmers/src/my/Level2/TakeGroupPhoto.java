feb83ba3424a863069f1311691684df148d354e0
m16khb <m16khb@gmail.com>
2022-02-01 17:27:18 +0900

    단체사진 찍기

---
+++ Programmers/src/my/Level2/TakeGroupPhoto.java
@@ -0,0 +1,15 @@
+package my.Level2;
+
+public class TakeGroupPhoto {
+
+	public static void main(String[] args) {
+		TakeGroupPhoto tgp = new TakeGroupPhoto();
+		System.out.println(tgp.solution(2, new String[] { "N~F=0", "R~T>2" }));
+	}
+
+	public int solution(int n, String[] data) {
+		int answer = 0;
+
+		return answer;
+	}
+}
