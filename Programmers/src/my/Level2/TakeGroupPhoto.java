995134ab623a3bff3fd5653388f0b0fcf991c929
m16khb <m16khb@gmail.com>
2022-02-03 01:06:07 +0900

    단체 사진찍기(시간 초과)

--- Programmers/src/my/Level2/TakeGroupPhoto.java
+++ Programmers/src/my/Level2/TakeGroupPhoto.java
@@ -1,6 +1,8 @@
 package my.Level2;
 
 public class TakeGroupPhoto {
+	String[] friends;
+	int answer;
 
 	public static void main(String[] args) {
 		TakeGroupPhoto tgp = new TakeGroupPhoto();
@@ -8,14 +10,54 @@ public class TakeGroupPhoto {
 	}
 
 	public int solution(int n, String[] data) {
-		int answer = 0;
-		
-		
+		answer = 0;
+		friends = new String[] { "A", "C", "F", "J", "M", "N", "R", "T" };
+
+		boolean[] isInLine = new boolean[8];
+		DFS(data, isInLine, "");
 
 		return answer;
 	}
-	
-	public void DFS() {
+
+	public void DFS(String[] data, boolean[] isInLine, String inputNames) {
+		if (inputNames.length() == 8) {
+			if (checkCondition(inputNames, data)) {
+				answer++;
+			}
+			return;
+		}
+		// 모든 경우의 수 다 구하기
+		for (int i = 0; i < isInLine.length; i++) {
+			if (!isInLine[i]) {
+				isInLine[i] = true;
+				String name = inputNames + friends[i];
+				DFS(data, isInLine, name);
+				isInLine[i] = false;
+			}
+		}
+	}
+
+	public boolean checkCondition(String inputNames, String[] data) {
+		boolean flag = true;
+
 		
+		for (String condition : data) {
+			int n = inputNames.indexOf(condition.charAt(0));
+			int m = inputNames.indexOf(condition.charAt(2));
+			if(condition.charAt(3) == '=') {
+				if(Math.abs(n - m) != (Character.getNumericValue(condition.charAt(4)) + 1))
+					flag = false;
+			}
+			else if(condition.charAt(3) == '<') {
+				if(Math.abs(n - m) >= (Character.getNumericValue(condition.charAt(4)) + 1))
+					flag = false;
+			}
+			else if(condition.charAt(3) == '>') {
+				if(Math.abs(n - m) <= (Character.getNumericValue(condition.charAt(4)) + 1))
+					flag = false;
+			}
+		}
+
+		return flag;
 	}
 }
